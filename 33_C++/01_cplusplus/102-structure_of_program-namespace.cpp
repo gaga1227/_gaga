66dc9e1e34306680785f722fe3b19ec652f53589
Johnny Xu <gaga1227@gmail.com>
2014-08-08 23:27:23 +1000

    added structure of program

---
+++ 33_C++/01_cplusplus/102-structure_of_program-namespace.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout << "Hello world! ";
+  cout << "I'm a C++ program";
+}
--- 33_C++/01_cplusplus/example.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-int main()
-{
-  auto x = R"(Hello world!)";
-  std::cout << x;
-}
