496be73f67598ad6f20bc2fe55e301c7d26aacd9
Johnny Xu <johnny.xu@sportsbet.com.au>
2014-09-17 17:06:27 +1000

    starting comments app

---
+++ 28_angular/03_planningforaliens/lib/comments.js
@@ -0,0 +1,28 @@
+(function(){
+
+	//def module
+	var app = angular.module('angular-demo', []);
+
+	//register directive to module
+	app.directive('comments', function(){
+		return {
+			restrict: 'AE',
+			controller: ['$scope', function($scope){
+				//model
+				$scope.comments = [
+					{
+						"text": "Comments 1"
+					},
+					{
+						"text": "Comments 2"
+					},
+					{
+						"text": "Comments 3"
+					}
+				];
+			}],
+			templteUrl: 'partial/comments.html'
+		}
+	});
+
+})();
