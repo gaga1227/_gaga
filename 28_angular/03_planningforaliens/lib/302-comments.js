4bb0fea6b644e9c1dd95dc618e4f6ff9ee5a1348
Johnny Xu <johnny.xu@sportsbet.com.au>
2014-09-23 13:21:05 +1000

    added firebase intro and promise with $http

---
+++ 28_angular/03_planningforaliens/lib/302-comments.js
@@ -0,0 +1,48 @@
+(function(){
+
+	//def module
+	var app = angular.module('angular-demo', ['ngSanitize', 'firebase']);
+
+	//register app constants
+	app.constant('FIREBASE_URL', 'https://angular-escape-plan.firebaseio.com');
+
+	//register directive to module
+	app.directive('comments', function(){
+		return {
+			restrict: 'AE',
+			controller: ['$scope', '$firebase', 'FIREBASE_URL', function($scope, $firebase, FIREBASE_URL){
+				//firebase
+				var ref = new Firebase(FIREBASE_URL);
+				var sync = $firebase(ref);
+
+				//comments local models
+				$scope.comments = sync.$asArray();
+
+				//textarea view model
+				$scope.commentMd = '';
+
+				//methods
+				$scope.post = function(){
+					//validate
+					if ($scope.commentMd == '') {
+						alert('Empty post message!');
+						return;
+					}
+					//add to model
+					$scope.comments.$add({ "md": $scope.commentMd });
+					//reset textarea view model
+					$scope.commentMd = '';
+				};
+			}],
+			templateUrl: 'partial/301-comments.html'
+		}
+	});
+
+	//register filter
+	app.filter('mdToHtml', function(){
+		return function(input){
+			return markdown.toHTML(input);
+		};
+	});
+
+})();
