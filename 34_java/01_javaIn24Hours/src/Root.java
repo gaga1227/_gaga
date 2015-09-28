0082a13084984e4ffc3a4e049d783ecf5afadbfd
Johnny Xu <johnny.xu@sportsbet.com.au>
2015-09-28 17:33:25 +1000

    hour4 - starting

---
+++ 34_java/01_javaIn24Hours/src/Root.java
@@ -0,0 +1,11 @@
+public class Root {
+	public static void main(String[] arguments) {
+		int number = 225;
+		System.out.println(
+			"The square root of " +
+			number +
+			" is " +
+			Math.sqrt(number)
+		);
+	}
+}
