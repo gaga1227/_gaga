2f3c725d4c16537f00df59b52fc1396683ed3d2c
Johnny Xu <gaga1227@gmail.com>
2017-08-19 17:24:32 +1000

    modernReactWithRedux - 19_-_Breather_and_Review

--- 35_react.js/02_modernReactWithRedux/ReduxSimpleStarter/src/components/search_bar.js
+++ 35_react.js/02_modernReactWithRedux/ReduxSimpleStarter/src/components/search_bar.js
@@ -2,17 +2,55 @@
  * search bar component
  */
 
-// ES6: exports module default to React and also exports 'Component' property from React
+// ES6: 
+// - exports module default to React
+// - also exports 'Component' property from React via object destructuring assignment syntax
 import React, { Component } from 'react';
 
 // React:
 // - class based component inherits React.Component
 // ES6: class
 class SearchBar extends Component {
+	// ES6: class constructor
+	// React: initialise with props
+	constructor(props) {
+		super(props);
+
+		// React:
+		// - 'state' is a plain JS object exists on class based component instances
+		// - only class based components can have state
+		// - initialise state obj in constructor
+		this.state = {
+			term : '',
+			placeholder: 'Type something...'
+		}
+	}
+	
 	// ES6: class method
 	// React: class based component must contain 'render' method that returns react.element
 	render() {
-		return <input />;
+		// JSX:
+		// - assign state value to 'value' prop makes 'input' a controlled component
+		// - pass event handler method ref as html prop value
+		return (
+			<div>
+				<input
+					value={this.state.term}
+					placeholder={this.state.placeholder}
+					onChange={this.onInputChange.bind(this)}
+					/>
+			</div>
+		)
+	}
+
+	// event handler method
+	onInputChange(e) {
+		// React:
+		// - should always use 'setState' to update state
+		// - 'setState' informs React state changes, might or might not trigger render
+		this.setState({
+			term: e.target.value
+		});
 	}
 }
 
