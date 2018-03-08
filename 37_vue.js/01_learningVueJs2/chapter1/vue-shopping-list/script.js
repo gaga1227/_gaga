8af427f5bd241e12ca5cbd892290899d382d18f7
Johnny Xu <johnny.xu@auspost.com.au>
2018-03-08 17:06:14 +1100

    learningVueJs2 - chap1 - adding examples

---
+++ 37_vue.js/01_learningVueJs2/chapter1/vue-shopping-list/script.js
@@ -0,0 +1,24 @@
+var data = {
+  items: [{ text: 'Bananas', checked: true }, { text: 'Apples', checked: false }],
+  title: 'My Shopping List',
+  newItem: ''
+};
+
+new Vue({ // jshint ignore:line
+  el: '#app',
+  data: data,
+  methods: {
+    addItem: function () {
+      var text; // jshint ignore:line
+
+      text = this.newItem.trim();
+      if (text) {
+        this.items.push({
+          text: text,
+          checked: false
+        });
+        this.newItem = '';
+      }
+    }
+  }
+});
