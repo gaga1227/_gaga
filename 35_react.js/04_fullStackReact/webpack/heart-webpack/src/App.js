c404b63719a593e85b26aefe5cc568b525c8ee4b
Johnny Xu <johnny.xu@auspost.com.au>
2018-12-02 16:57:03 +1100

    fullstackreact - 1.7 - wip

---
+++ 35_react.js/04_fullStackReact/webpack/heart-webpack/src/App.js
@@ -0,0 +1,25 @@
+// inform webpack to import npm packages
+import React, { Component } from 'react';
+
+// inform webpack of all dependencies using this syntax
+// webpack knows we are referring to local files based on relative paths
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
