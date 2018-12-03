6d29b38fb3b5b9b6f603d26d54b89db0095f29f0
Johnny Xu <gaga@JX-MBP.local>
2018-12-03 22:36:46 +1100

    fullstackreact - 1.8.5 - wip

--- 35_react.js/04_fullStackReact/testing/react-basics/src/App.test.js
+++ 35_react.js/04_fullStackReact/testing/react-basics/src/App.test.js
@@ -1,8 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'; // need this due to using JSX -> React.createElement()
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+// using shallow renderer from enzyme instead of ReactDom renderer
+import { shallow } from 'enzyme';
+
+describe('App', () => {
+  it('should have the `th` "Items"', () => {
+    const wrapper = shallow(<App/>);
+    expect(wrapper.contains(<th>Items</th>)).toBe(true); // contains accepts ReactElement, so can use JSX
+  });
 });
--- 35_react.js/04_fullStackReact/testing/react-basics/src/setupTests.js
+++ 35_react.js/04_fullStackReact/testing/react-basics/src/setupTests.js
@@ -1,7 +1,10 @@
+// Note: Create React App configures Jest so that this file is loaded automatically before each test suite run
+
 // temporary fix for React 16 via https://github.com/facebookincubator/create-react-app/issues/3199#issuecomment-334801311
 import raf from './tempPolyfills'
 
 import Enzyme  from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
+// configure enzyme with proper adaptor matching react version
 Enzyme.configure({ adapter: new Adapter() });
