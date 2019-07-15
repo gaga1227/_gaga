0a6f2179e368ac585b2e7db5dad546168a93645e
Johnny Xu <gaga@JX-MBP.local>
2019-07-15 19:59:07 +1000

    acg | to 22

--- 28_angular/04_angularTheCompleteGuide/my-first-app/src/app/app.module.ts
+++ 28_angular/04_angularTheCompleteGuide/my-first-app/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ServersComponent } from './servers/servers.component';
+import { ServerComponent } from './server/server.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServersComponent,
+    ServerComponent
   ],
   imports: [
     BrowserModule
