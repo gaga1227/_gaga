1572d4a3e224b9642ea9446fb30a506de454f65b
Johnny Xu <gaga@JX-MBP.local>
2019-07-16 20:28:56 +1000

    acg | to 26

--- 28_angular/04_angularTheCompleteGuide/my-first-app/src/app/servers/servers.component.ts
+++ 28_angular/04_angularTheCompleteGuide/my-first-app/src/app/servers/servers.component.ts
@@ -4,15 +4,22 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-servers',
   // selector: '[app-servers]',
   // selector: '.app-servers',
-  template: `
-    <app-server></app-server>
-    <app-server></app-server>
-  `,
+  // template: `
+  //   <app-server></app-server>
+  //   <app-server></app-server>
+  // `,
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
 
-  constructor() { }
+  allowNewServer = false;
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
   ngOnInit() {
   }
