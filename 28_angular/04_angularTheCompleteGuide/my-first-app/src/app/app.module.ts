325b18578fa4c0b409e2c376ba8e80fb7703698e
Johnny Xu <gaga@JX-MBP.local>
2019-07-17 19:00:08 +1000

    acg | to 31

--- 28_angular/04_angularTheCompleteGuide/my-first-app/src/app/app.module.ts
+++ 28_angular/04_angularTheCompleteGuide/my-first-app/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ServersComponent } from './servers/servers.component';
@@ -12,7 +13,8 @@ import { ServerComponent } from './server/server.component';
     ServerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- 28_angular/04_angularTheCompleteGuide/my-first-app/src/app/servers/servers.component.ts
+++ 28_angular/04_angularTheCompleteGuide/my-first-app/src/app/servers/servers.component.ts
@@ -14,6 +14,8 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
 
   allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = '';
 
   constructor() {
     setTimeout(() => {
@@ -24,4 +26,11 @@ export class ServersComponent implements OnInit {
   ngOnInit() {
   }
 
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created!';
+  }
+
+  // onUpdateServerName(event: any) {
+  //   this.serverName = event.target.value;
+  // }
 }
