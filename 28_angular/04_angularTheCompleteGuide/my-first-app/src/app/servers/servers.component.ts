6cce5329728bb73c2666680f99fdf454e08fef8c
Johnny Xu <gaga@JX-MBP.local>
2019-07-17 19:03:40 +1000

    acg | to 33

--- 28_angular/04_angularTheCompleteGuide/my-first-app/src/app/servers/servers.component.ts
+++ 28_angular/04_angularTheCompleteGuide/my-first-app/src/app/servers/servers.component.ts
@@ -15,7 +15,7 @@ export class ServersComponent implements OnInit {
 
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
-  serverName = '';
+  serverName = 'Test Server';
 
   constructor() {
     setTimeout(() => {
@@ -27,7 +27,7 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
-    this.serverCreationStatus = 'Server was created!';
+    this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
   }
 
   // onUpdateServerName(event: any) {
