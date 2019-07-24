87bfa6bb25904cde4842b5dd772fa3409f859b27
Johnny Xu <gaga@JX-MBP.local>
2019-07-24 19:45:41 +1000

    acg | to 113
    - using @Injectable decorator

--- 28_angular/04_angularTheCompleteGuide/sec9-app/src/app/accounts.service.ts
+++ 28_angular/04_angularTheCompleteGuide/sec9-app/src/app/accounts.service.ts
@@ -1,8 +1,11 @@
 import { LoggingService } from './logging.service';
 import { Injectable } from '@angular/core';
 
-// add decorator if you want to DI into this class
-@Injectable()
+// add decorator if you want to DI into this class, or make this class injectable
+// this enables services can be lazy loaded and injected, avaiable in angular 6+
+@Injectable({
+  providedIn: 'root'
+})
 export class AccountsService {
   // shared data for accounts services across components
   accounts = [
--- 28_angular/04_angularTheCompleteGuide/sec9-app/src/app/app.module.ts
+++ 28_angular/04_angularTheCompleteGuide/sec9-app/src/app/app.module.ts
@@ -6,8 +6,6 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { AccountComponent } from './account/account.component';
 import { NewAccountComponent } from './new-account/new-account.component';
-import { AccountsService } from './accounts.service';
-import { LoggingService } from './logging.service';
 
 @NgModule({
   declarations: [
@@ -19,7 +17,7 @@ import { LoggingService } from './logging.service';
     BrowserModule,
     FormsModule
   ],
-  providers: [AccountsService, LoggingService], // app module wide singletons, add LoggingService here so it can be DI into AccountsServices
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {
