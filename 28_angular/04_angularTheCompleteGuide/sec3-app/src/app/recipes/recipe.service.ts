7bcfcd7190162a9e344f3c0b0196c5e04e1ba62e
Johnny Xu <gaga@JX-MBP.local>
2019-07-25 19:50:36 +1000

    acg | to 120

--- 28_angular/04_angularTheCompleteGuide/sec3-app/src/app/recipes/recipe.service.ts
+++ 28_angular/04_angularTheCompleteGuide/sec3-app/src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import { Recipe } from './recipe.model';
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 // @Injectable({
 //   providedIn: 'root'
@@ -11,11 +12,20 @@ export class RecipeService {
     new Recipe(
       'A test recipe',
       'simply test',
-      'https://www.seriouseats.com/recipes/images/20110408-soup-dumpling11.jpg'),
+      'https://www.seriouseats.com/recipes/images/20110408-soup-dumpling11.jpg',
+      [
+        new Ingredient('flour', 2),
+        new Ingredient('minced pork', 1)
+      ]),
     new Recipe(
       'A test recipe 2',
       'simply test 2',
-      'https://img.kidspot.com.au/_c5yRpt-/kk/2015/03/5209-501430-1.jpg')
+      'https://img.kidspot.com.au/_c5yRpt-/kk/2015/03/5209-501430-1.jpg',
+      [
+        new Ingredient('rice', 5),
+        new Ingredient('egg', 1),
+        new Ingredient('pork', 1)
+      ])
   ];
 
   constructor() {
