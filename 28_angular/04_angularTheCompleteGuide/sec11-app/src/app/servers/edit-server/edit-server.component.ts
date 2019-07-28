c7fddc56695e8ba04948b4af572aff270fbce61c
Johnny Xu <gaga@JX-MBP.local>
2019-07-28 15:37:33 +1000

    acg | to 136
    - get route query param and fragment

--- 28_angular/04_angularTheCompleteGuide/sec11-app/src/app/servers/edit-server/edit-server.component.ts
+++ 28_angular/04_angularTheCompleteGuide/sec11-app/src/app/servers/edit-server/edit-server.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-edit-server',
@@ -8,13 +9,22 @@ import { ServersService } from '../servers.service';
   styleUrls: ['./edit-server.component.css']
 })
 export class EditServerComponent implements OnInit {
-  server: {id: number, name: string, status: string};
+  server: { id: number, name: string, status: string };
   serverName = '';
   serverStatus = '';
 
-  constructor(private serversService: ServersService) { }
+  constructor(
+    private serversService: ServersService,
+    private route: ActivatedRoute) {
+  }
 
   ngOnInit() {
+    console.log(this.route.snapshot.queryParams);
+    console.log(this.route.snapshot.fragment);
+
+    this.route.queryParams.subscribe();
+    this.route.fragment.subscribe();
+
     this.server = this.serversService.getServer(1);
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
