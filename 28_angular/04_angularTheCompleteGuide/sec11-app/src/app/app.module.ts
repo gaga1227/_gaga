bcf98c2064d3520a0d8f6d734dcfab7b66270d57
Johnny Xu <gaga@JX-MBP.local>
2019-07-28 19:08:15 +1000

    acg | to 138
    - child/nested routes

--- 28_angular/04_angularTheCompleteGuide/sec11-app/src/app/app.module.ts
+++ 28_angular/04_angularTheCompleteGuide/sec11-app/src/app/app.module.ts
@@ -13,30 +13,16 @@ import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
 
 const appRoutes: Routes = [
-  {
-    path: '', // default route
-    component: HomeComponent
-  },
-  {
-    path: 'users',
-    component: UsersComponent
-  },
-  {
-    path: 'users/:id/:name',
-    component: UserComponent
-  },
-  {
-    path: 'servers',
-    component: ServersComponent
-  },
-  {
-    path: 'servers/:id',
-    component: ServerComponent
-  },
-  {
-    path: 'servers/:id/edit',
-    component: EditServerComponent
-  }
+  {path: '', component: HomeComponent},
+  {path: 'users', component: UsersComponent,
+    children: [
+      {path: ':id/:name', component: UserComponent}
+    ]},
+  {path: 'servers', component: ServersComponent,
+    children: [
+      {path: ':id', component: ServerComponent},
+      {path: ':id/edit', component: EditServerComponent}
+    ]}
 ];
 
 @NgModule({
