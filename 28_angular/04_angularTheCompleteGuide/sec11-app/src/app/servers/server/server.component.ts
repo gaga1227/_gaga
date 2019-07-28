c2d25a116faf03dec290a6e23be778b079682a65
Johnny Xu <gaga@JX-MBP.local>
2019-07-28 16:02:10 +1000

    acg | to 137

--- 28_angular/04_angularTheCompleteGuide/sec11-app/src/app/servers/server/server.component.ts
+++ 28_angular/04_angularTheCompleteGuide/sec11-app/src/app/servers/server/server.component.ts
@@ -1,19 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
   styleUrls: ['./server.component.css']
 })
-export class ServerComponent implements OnInit {
-  server: {id: number, name: string, status: string};
+export class ServerComponent implements OnInit, OnDestroy {
+  server: { id: number, name: string, status: string };
+  private paramSubscription: Subscription;
 
-  constructor(private serversService: ServersService) { }
+  constructor(
+    private serversService: ServersService,
+    private route: ActivatedRoute
+  ) {
+  }
 
   ngOnInit() {
-    this.server = this.serversService.getServer(1);
+    this.paramSubscription = this.route.params
+      .subscribe((params: Params) => {
+        const id = (params && params.id) || 1;
+        this.server = this.serversService.getServer(parseInt(id, 10));
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.paramSubscription.unsubscribe();
   }
 
 }
