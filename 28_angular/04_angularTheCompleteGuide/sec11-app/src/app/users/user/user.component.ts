ea57e514fce6956e6b2fa1274d0f6b2615dc889e
Johnny Xu <gaga@JX-MBP.local>
2019-07-28 15:17:23 +1000

    acg | to 134
    - ActivatedRoute.params: Observables
    - unsubscribe any subscription in component lifecycle

--- 28_angular/04_angularTheCompleteGuide/sec11-app/src/app/users/user/user.component.ts
+++ 28_angular/04_angularTheCompleteGuide/sec11-app/src/app/users/user/user.component.ts
@@ -1,22 +1,41 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
   user: { id: number, name: string };
+  private routeParamsSubscription: Subscription;
 
   constructor(private route: ActivatedRoute) {
   }
 
   ngOnInit() {
+    // ActivatedRoute is initialised when component initialised
+    // fine for first initialisation
+    // snapshot won't update it by itself
     this.user = {
       id: this.route.snapshot.params['id'],
       name: this.route.snapshot.params['name']
     };
+
+    // params are Observables, not values
+    // future updates
+    this.routeParamsSubscription = this.route.params
+      .subscribe((params: Params) => {
+        this.user = {
+          id: params['id'],
+          name: params['name']
+        };
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.routeParamsSubscription.unsubscribe();
   }
 
 }
