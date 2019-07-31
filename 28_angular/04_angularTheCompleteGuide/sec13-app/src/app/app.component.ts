5d369b7e2b20e9df9efeb46e9f7ee115b9a5d769
Johnny Xu <gaga@JX-MBP.local>
2019-07-31 20:46:02 +1000

    acg | to 177
    - subscribe to button events using eventEmitter

--- 28_angular/04_angularTheCompleteGuide/sec13-app/src/app/app.component.ts
+++ 28_angular/04_angularTheCompleteGuide/sec13-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  constructor() {}
+  userActivated = false;
 
-  ngOnInit() {}
+  constructor(private userService: UserService) {
+  }
+
+  ngOnInit() {
+    this.userService.activatedEmitter
+      .subscribe((activateStatus: boolean) => {
+        this.userActivated = activateStatus;
+      });
+  }
 }
---
+++ 28_angular/04_angularTheCompleteGuide/sec13-app/src/app/user.service.ts
@@ -0,0 +1,8 @@
+import { EventEmitter, Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  activatedEmitter = new EventEmitter<boolean>();
+}
