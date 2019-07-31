aa0021844111971aede20ce4e9de4a84e4314cc2
Johnny Xu <gaga@JX-MBP.local>
2019-07-31 19:44:18 +1000

    acg | to 170
    - creating own observable with interval

--- 28_angular/04_angularTheCompleteGuide/sec13-app/src/app/home/home.component.ts
+++ 28_angular/04_angularTheCompleteGuide/sec13-app/src/app/home/home.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  private intervalSubscription: Subscription;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
+    this.intervalSubscription = interval(1000)
+      .subscribe(
+        (count: number) => {
+          console.log(count);
+        },
+        (error: any) => {
+          console.log(error);
+        },
+        () => {
+          console.log('complete');
+        }
+      );
+  }
+
+  // subscriptions to non-angular observables won't be cleaned up on destroy,
+  // should manually unsubscribe
+  ngOnDestroy(): void {
+    this.intervalSubscription.unsubscribe();
   }
 
 }
