482d00903db7dae4fde5a795b1fadc833c75cae9
Johnny Xu <gaga@JX-MBP.local>
2019-08-01 19:48:03 +1000

    acg | to 187
    - using @viewchild to access form element via element ref

--- 28_angular/04_angularTheCompleteGuide/sec15-app/src/app/app.component.ts
+++ 28_angular/04_angularTheCompleteGuide/sec15-app/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -7,12 +7,22 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  // use @viewchild to get child form element via element ref name
+  @ViewChild('ngForm', {static: false})
+  ngForm: NgForm;
+
   suggestUserName() {
     const suggestedName = 'Superuser';
   }
 
-  // ngSubmit handler
+  // ngSubmit handler, ref is passed form template
   onSubmit(event: Event, form: HTMLFormElement, ngForm: NgForm) {
     console.log(event, form, ngForm);
+  }
+
+  // using ref from viewchild
+  onSubmitWithViewChild() {
+    console.log(this.ngForm);
   }
 }
